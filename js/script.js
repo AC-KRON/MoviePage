ca0f96ec26db07c9fa6ad991e3d44233b6dcece1
IPVM7 <ianvaldebenitom@gmail.com>
2023-04-12 22:04:05 -0400

    Added movies to webpage using the movie API

--- js/script.js
+++ js/script.js
@@ -12,13 +12,60 @@ function highlightActiveLink() {
   });
 }
 
-// Init App
+// Fetch data from TMDB API
+const fetchAPIData = async (endpoint) => {
+  const API_KEY = '05c9b2e6aa39ba730baaf8f6521316c0';
+  const API_URL = 'https://api.themoviedb.org/3/';
 
+  const response = await fetch(
+    `${API_URL}/${endpoint}?api_key=${API_KEY}&language=en-US`
+  );
+  const data = await response.json();
+  return data;
+};
+
+// Get Popular Movies
+
+const displayPopularMovies = async () => {
+  const { results } = await fetchAPIData('movie/popular');
+  console.log(results);
+  results.forEach((movie) => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+    <a href="movie-details.html?id=${movie.id}">
+      ${
+        movie.poster_path
+          ? `
+        <img
+        src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+        class="card-img-top"
+        alt="${movie.title}"
+        /> `
+          : `
+        
+        <img
+        src="../images/no-image.jpg"
+        class="card-img-top"
+        alt="${movie.title}"
+        />`
+      }
+        </a>
+        <h5 class="card-title">${movie.title}</h5>
+        <p class="card-text">
+          <small class="text-muted">Release: ${movie.release_date}</small>
+        </p>`;
+
+    document.querySelector('#popular-movies').appendChild(div);
+  });
+};
+
+// Init App
 function init() {
   switch (global.currentPage) {
     case '/':
     case '/index.html':
-      console.log('Home');
+      displayPopularMovies();
       break;
     case '/shows.html':
       console.log('Shows');
