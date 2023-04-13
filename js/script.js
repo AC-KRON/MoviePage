2e7fca92b8b9194c53b3595e27e5b696d911a8fe
IPVM7 <ianvaldebenitom@gmail.com>
2023-04-13 00:18:38 -0400

    Added Movie Details webpage with API data

--- js/script.js
+++ js/script.js
@@ -69,6 +69,75 @@ const displayPopularMovies = async () => {
   });
 };
 
+// Display Movie Details
+const displayMovieDetails = async () => {
+  const movieId = window.location.search.split('=')[1];
+  const movie = await fetchAPIData(`movie/${movieId}`);
+
+  const div = document.createElement('div');
+  div.innerHTML = `
+  <div class="details-top">
+          <div>
+          ${
+            movie.poster_path
+              ? `
+            <img
+            src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+            class="card-img-top"
+            alt="${movie.title}"
+            /> `
+              : `
+            
+            <img
+            src="../images/no-image.jpg"
+            class="card-img-top"
+            alt="${movie.title}"
+            />`
+          }
+          </div>
+          <div>
+            <h2>${movie.title}</h2>
+            <p>
+              <i class="fas fa-star text-primary"></i>
+              ${movie.vote_average.toFixed(1)} / 10
+            </p>
+            <p class="text-muted">Release Date: ${movie.release_date}</p>
+            <p>
+              ${movie.overview}
+            </p>
+            <h5>Genres</h5>
+            <ul class="list-group">
+              ${movie.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+            </ul>
+            <a href="${
+              movie.homepage
+            }" target="_blank" class="btn">Visit Movie Homepage</a>
+          </div>
+        </div>
+        <div class="details-bottom">
+          <h2>Movie Info</h2>
+          <ul>
+            <li><span class="text-secondary">Budget:</span> $${addCommasToPrice(
+              movie.budget
+            )}</li>
+            <li><span class="text-secondary">Revenue:</span> $${addCommasToPrice(
+              movie.revenue
+            )}</li>
+            <li><span class="text-secondary">Runtime:</span> ${
+              movie.runtime
+            } minutes</li>
+            <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+          </ul>
+          <h4>Production Companies</h4>
+          <div class="list-group">${movie.production_companies
+            .map((company) => `<li>${company.name}.</li> `)
+            .join('')}</div>
+        </div>
+  `;
+
+  document.querySelector('#movie-details').appendChild(div);
+};
+
 const displayPopularShows = async () => {
   const { results } = await fetchAPIData('tv/popular');
   console.log(results);
@@ -103,6 +172,10 @@ const displayPopularShows = async () => {
   });
 };
 
+const addCommasToPrice = (number) => {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+};
+
 // Init App
 function init() {
   switch (global.currentPage) {
@@ -114,7 +187,7 @@ function init() {
       displayPopularShows();
       break;
     case '/movie-details.html':
-      console.log('Movies Details');
+      displayMovieDetails();
       break;
     case '/tv-details.html':
       console.log('Tv Details');
