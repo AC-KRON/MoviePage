0e183625429aa9a870475cabf7876cc88d9deeec
IPVM7 <ianvaldebenitom@gmail.com>
2023-04-15 16:10:54 -0400

    Added search logic

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
 const global = {
   currentPage: window.location.pathname,
+  search: {
+    term: '',
+    type: '',
+    page: 1,
+    totalPages: 1,
+  },
+  api: {
+    apiKey: '05c9b2e6aa39ba730baaf8f6521316c0',
+    apiUrl: 'https://api.themoviedb.org/3/',
+  },
 };
 
 // Highlight active link
@@ -14,11 +24,23 @@ function highlightActiveLink() {
 
 // Fetch data from TMDB API
 const fetchAPIData = async (endpoint) => {
-  const API_KEY = '05c9b2e6aa39ba730baaf8f6521316c0';
-  const API_URL = 'https://api.themoviedb.org/3/';
+  const API_KEY = global.api.apiKey;
+  const API_URL = global.api.apiUrl;
   showSpinner();
   const response = await fetch(
     `${API_URL}/${endpoint}?api_key=${API_KEY}&language=en-US`
+  );
+  const data = await response.json();
+  hideSpinner();
+  return data;
+};
+
+const searchAPIData = async () => {
+  const API_KEY = global.api.apiKey;
+  const API_URL = global.api.apiUrl;
+  showSpinner();
+  const response = await fetch(
+    `${API_URL}/search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}`
   );
   const data = await response.json();
   hideSpinner();
@@ -296,7 +318,24 @@ const initSwiper = () => {
 const search = async () => {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
-  console.log(urlParams);
+  global.search.type = urlParams.get('type');
+  global.search.term = urlParams.get('search-term');
+
+  if (global.search.term !== '' && global.search.term !== null) {
+    const results = await searchAPIData();
+    console.log(results);
+  } else {
+    showAlert('Please enter search term');
+  }
+};
+
+const showAlert = (message, className) => {
+  const alertEl = document.createElement('div');
+  alertEl.classList.add('alert', className);
+  alertEl.appendChild(document.createTextNode(message));
+  document.querySelector('#alert').appendChild(alertEl);
+
+  setTimeout(() => alertEl.remove(), 3000);
 };
 
 const addCommasToPrice = (number) => {
